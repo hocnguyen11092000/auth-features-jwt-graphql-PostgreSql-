207af6e5eebe67588b5b3a5d1549c8f8793a7167
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-03-14 18:23:51 +0700

    update login file

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -4,7 +4,10 @@ const Profile = () => {
   const { data, error, loading } = useHelloQuery({ fetchPolicy: "no-cache" });
 
   if (loading) return <h3 style={{ textAlign: "center" }}>Loading...</h3>;
-  if (error) return <h3 style={{ textAlign: "center" }}>Error...</h3>;
+  if (error)
+    return (
+      <h3 style={{ textAlign: "center" }}>Please login to see profile...</h3>
+    );
 
   return <h1 style={{ textAlign: "center" }}>{`${data?.hello}`}</h1>;
 };
